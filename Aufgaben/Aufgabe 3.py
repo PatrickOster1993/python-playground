9c85a8c6adf3a8930ae5e66f82aa4ad75a1b5f93
Maze <1932172085@bildung.daa.de>
2025-01-14 14:14:55 +0100

    solved 'Aufgabe 3.py'

--- Aufgaben/Aufgabe 3.py
+++ Aufgaben/Aufgabe 3.py
@@ -4,3 +4,38 @@
 
 # > **Tipp**: Wie wäre es mit dem Modulo-Operator?
 
+##############################
+# FizzBuzz mit Modulo-Operator
+# for zahl in range(1, 101):
+#     if zahl % 3 == 0 and zahl % 5 == 0:
+#         print("FizzBuzz")
+#     elif zahl % 3 == 0:
+#         print("Fizz")
+#     elif zahl % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(zahl)
+
+###############################
+# FizzBuzz ohne Modulo-Operator
+zähler_3 = 0
+zähler_5 = 0
+
+for zahl in range(1, 101):
+    zähler_3 += 1
+    zähler_5 += 1
+    
+    ausgabe = ""
+    
+    if zähler_3 == 3:
+        ausgabe += "Fizz"
+        zähler_3 = 0  
+    
+    if zähler_5 == 5:
+        ausgabe += "Buzz"
+        zähler_5 = 0  
+    
+    if not ausgabe:
+        ausgabe = str(zahl)
+    
+    print(ausgabe)
