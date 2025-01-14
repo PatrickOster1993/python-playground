77fcffc22b42b0809c63cc66e2c856e362565a3f
Schneider <schneideralex318@gmail.com>
2025-01-14 14:39:26 +0100

    Aufgaben 1 und 2 separat in Ordner gepackt

---
+++ Aufgaben 14.01.25/Aufgabe1.py
@@ -0,0 +1,6 @@
+# Aufgabe 1
+
+Name = input("Wie heißt du?")
+Alter = input("Wie alt bist du?")
+Essen = input("Was ist dein Lieblingsessen?")
+print("Hallo", Name, ",du bist", Alter, "Jahre alt und dein Lieblingsessen ist", Essen)
