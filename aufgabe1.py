fe826b0ae8c114fadb55eb9739c3758a82cecee8
benediktbrenk <benedikt.brenk@outlook.de>
2025-01-14 14:58:12 +0100

    Aufgabe 1

---
+++ aufgabe1.py
@@ -0,0 +1,5 @@
+name = input("Bitte geben Sie Ihren Namen ein: ")
+age = input("Bitte geben Sie Ihr Alter ein: ")
+favourite_meal = input("Bitte geben Sie Ihr Lieblingsessen ein: ")
+
+print(f"Hallo {name}, du bist {age} Jahre alt und dein Lieblingsessen ist {favourite_meal}.")
