c3c72d5eddaa1d18bce93e0410dbd60d71e643b2
GentheUgly <roy.faust.b@gmail.com>
2025-01-14 23:04:20 +0100

    Update Aufgabe_1 + Kommentaren

--- Aufgaben/Aufgabe_1.py
+++ Aufgaben/Aufgabe_1.py
@@ -1,6 +1,16 @@
-vorname = input("Bitte geben Sie ihren Vornamen an ")
-nachname = input("Bitte geben Sie ihren Nachnamen an ")
-alter = input("Wie Alt sind Sie? ")
-lieblingsessen = input("Was ist ihr Lieblingsessen? ")
+"""
+Aufgabe 1 Erstellen Sie ein Programm, das Ihren **Namen**, Ihr **Alter** und Ihr **Lieblingsessen** 
+über eine Konsoleneingabe einliest. Geben Sie diese Informationen dann in einem Satz aus.
+"""
+#Variablen
+vorname =           input("Bitte geben Sie ihren Vornamen an! ")
+nachname =          input("Bitte geben Sie ihren Nachnamen an! ")
+alter =             input("Wie Alt sind Sie? Ausgeschrieben oder als Zahl! ")
+lieblingsessen =    input("Was ist ihr Lieblingsessen? ")
 
-print(f"Grüß Gott! {vorname} {nachname}, du bist {alter} Jahre alt und dein Lieblingsessen ist {lieblingsessen}.")
+#Die Ausgabe mit den Variablen
+print(f"Grüß Gott! {vorname} {nachname}, du bist {alter} Jahre alt, dein Lieblingsessen ist {lieblingsessen}.")
+"""
+print(f"text") (formatierter string) funktioniert genau so wie in C# mit Console.Writeline($"text") (interpolierter String)
+formatierter string und interpolierter string Variablen und Ausdrücke direkt ausgewertet.
+"""
