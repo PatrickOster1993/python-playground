70f4d41771bb5a4c7c7badda80d57b61f20f3747
GentheUgly <roy.faust.b@gmail.com>
2025-01-14 23:30:51 +0100

    Aufgabe_2 update

--- Aufgaben/Aufgabe_2.py
+++ Aufgaben/Aufgabe_2.py
@@ -7,4 +7,29 @@ mit F: Temperatur in **Fahrenheit**;
 sowie C: Temperatur in **Celsius**
 
 > **Anmerkung**: Geben Sie das Ergebnis bitte als Antwortsatz aus!
+
+21Quelle
+https://stackoverflow.com/questions/69171137/convert-fahrenheit-to-celsius-python
+"""
+
+# Temperatur von Celsius in Fahrenheit
+
+celsius = float(input("Gib eine Temperatur in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+#ausgabe 
+#print(f"{celsius} Celsius sind: {fahrenheit} Fahrenheit") Altvernative schriebweise wie in Aufgabe_1 (C# ähnlich write.line($))
+print(str(celsius) + " Celsius sind: " + str(fahrenheit) + " Fahrenheit")
+
+#eine leere Zeile einfügen für die Übersicht
+print()
+
+# Temperatur von Fahrenheit nach Celsius
+fahrenheit = float(input("Gib eine Temperarur in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(f"{fahrenheit} Fahrenheit sind: {celsius:.0f} Celsius") #Alternativeschreibweise hier funktioniert {celsius:.0f}!!!
+#print(str(fahrenheit) + " Fahrenheit sind: " + str(celsius:.0f) + " Celsius")
+
+"""
+fun fact 33.8 Fahrenheit sind ist Aufgrund der Gleitkommazahlen (Binäre Rechnung) nicht 1 sondern 0.9999999999999984 im PC
+man kann mit {celsius.0f} auf keine stelle hinterm komma die zahl runden lassen. 
 """
