a6abee5719b86a356f2cf4481cc78a0b064cc0ad
GentheUgly <roy.faust.b@gmail.com>
2025-01-15 00:11:23 +0100

    Aufgabe_4 bearbeitet, ausgeführt und snake.txt dadurch erstellt

--- Aufgaben/Aufgabe_4.py
+++ Aufgaben/Aufgabe_4.py
@@ -7,3 +7,26 @@ Text dann in einer **snake.txt** abspeichert.
 > *Hinweis*: Überlegen Sie sich eine sinnvolle Möglichkeit, wie Sie auch Texte über mehrere Zeilen eingeben können!
 """
 
+# Nutzer auffordern, Text einzugeben
+print("Geben Sie Ihren Text ein (mehrzeilig möglich).")
+print("Drücken Sie ENTER nach jeder Zeile und eine leere Zeile, um die Eingabe zu beenden.")
+
+# Text einlesen
+text = ""  # Leerer String, um den Text zu speichern
+while True:
+    line = input()  # Zeile vom Nutzer einlesen
+    if line == "":  # Wenn die Eingabe leer ist, die Schleife beenden
+        break
+    text += line + "\n"  # Zeile zum Text hinzufügen, mit Zeilenumbruch
+
+# Punkte durch "SNAKE_WAS_HERE" ersetzen
+text = text.replace(".", "SNAKE_WAS_HERE")
+
+# In Datei speichern
+with open("snake.txt", "w") as file:
+    file.write(text)
+
+# Erfolgsnachricht
+print("Der bearbeitete Text wurde in 'snake.txt' gespeichert!")
+
+""""""
