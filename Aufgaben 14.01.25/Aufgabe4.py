6809eb252d4fd030bd01418b0c95a283533be73f
Schneider <schneideralex318@gmail.com>
2025-01-14 15:25:16 +0100

    Aufgabe 4 ohne snake.txt Speicherung

--- Aufgaben 14.01.25/Aufgabe4.py
+++ Aufgaben 14.01.25/Aufgabe4.py
@@ -3,3 +3,10 @@
 # über die Konsole einliest, jeden **"." (Punkt)** durch **"SNAKE_WAS_HERE"** ersetzt und
 # den bearbeiteten Text dann in einer **snake.txt** abspeichert.
 
+user_input = input("Tragen Sie Ihre Gedanken ein.")
+
+neuer_text = user_input.replace(".", "SNAKE_WAS_HERE")
+
+print("Ersetzter Text:", neuer_text)
+
+#muss noch in snake.txt gespeichert werden 
