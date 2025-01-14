3662ff1eb26f2404facbb672fbe0028697fe905f
Maze <1932172085@bildung.daa.de>
2025-01-14 15:00:34 +0100

    changed output path 'Aufgabe 4.py'

--- Aufgaben/Aufgabe 4.py
+++ Aufgaben/Aufgabe 4.py
@@ -23,12 +23,11 @@ while True:
 modified_text = text.replace(".", "SNAKE_WAS_HERE")
 
 # Ändern des Pfades: Hier wird die Datei im spezifischen Verzeichnis gespeichert
-output_path = "c:/Users/IT-User/python-playground/Aufgaben/snake.txt"
 
 # Speichern des bearbeiteten Textes in der Datei
 # Der bearbeitete Text wird in einer Datei namens snake.txt gespeichert
 # Die Datei wird im Schreibmodus ("w") geöffnet. Falls die Datei bereits existiert, wird sie überschrieben
-with open(output_path, "w") as file:
+with open("snake.txt", "w") as file:
     file.write(modified_text)
 
 print("Text wurde erfolgreich bearbeitet und in 'snake.txt' gespeichert.")
