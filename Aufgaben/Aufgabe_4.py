343bfcc9a429e464807c5cb066ed22d541febbda
GentheUgly <roy.faust.b@gmail.com>
2025-01-15 00:02:34 +0100

    Aufgabe_4.py erstellt

---
+++ Aufgaben/Aufgabe_4.py
@@ -0,0 +1,9 @@
+"""
+Aufgabe 4
+Schreiben Sie eine Funktion, die Ihnen einen längeren Text (z. B. die im heutigen Unterricht erlernten Themen) 
+über die Konsole einliest, jeden **"." (Punkt)** durch **"SNAKE_WAS_HERE"** ersetzt und den bearbeiteten 
+Text dann in einer **snake.txt** abspeichert.
+
+> *Hinweis*: Überlegen Sie sich eine sinnvolle Möglichkeit, wie Sie auch Texte über mehrere Zeilen eingeben können!
+"""
+
