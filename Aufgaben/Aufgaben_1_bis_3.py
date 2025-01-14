4543849f43ad4fc4b7996cee8b31799cade732dc
Maze <1932172085@bildung.daa.de>
2025-01-14 13:26:08 +0100

    Aufgabe 1 solved

---
+++ Aufgaben/Aufgaben_1_bis_3.py
@@ -0,0 +1,18 @@
+# ### Aufgabe 1
+# Erstellen Sie ein Programm, das Ihren **Namen**, Ihr **Alter** und Ihr **Lieblingsessen** über eine Konsoleneingabe einliest. Geben Sie diese Informationen dann in einem Satz aus.
+
+# > z. B.: „Hallo Max, du bist 25 Jahre alt und dein Lieblingsessen ist Pizza.“
+
+# Eingabe vom Nutzer
+benutzer_input_name = input("Geben Sie Ihren Namen ein: ")
+benutzer_input_alter = input("Geben Sie Ihr Alter ein: ")
+benutzer_input_essen = input("Geben Sie Ihr Lieblingsessen ein: ")
+# F-String
+print(f"Hallo {benutzer_input_name}, du bist {benutzer_input_alter} Jahre alt und dein Lieblingsessen ist {benutzer_input_essen}.")
+
+# String-Konkatenation
+print("Hallo " + benutzer_input_name + ", du bist " + str(benutzer_input_alter) + " Jahre alt und dein Lieblingsessen ist " + benutzer_input_essen + ".")
+
+# sep-Parameter
+print("Hallo ", benutzer_input_name, ", du bist ", benutzer_input_alter, " Jahre alt und dein Lieblingsessen ist ", benutzer_input_essen, ".", sep="")
+
