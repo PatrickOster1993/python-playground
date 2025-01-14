d135528ad9c6167a4154f8eca356b4ad9ee020e1
Mattijn <mattijnwitteveen@gmail.com>
2025-01-14 14:42:59 +0100

    lösung aufgabe 1

---
+++ selbstlernphase.py
@@ -0,0 +1,5 @@
+name = input("Gib deinen Namen ein: ")
+age = input("Gib dein Alter ein: ")
+fav_food = input("Gib dein Lieblingsessen ein: ")
+
+print(f"Hallo {name}, wie gehts? Hast du die letzte Zeit nog {fav_food} gegessen oder bist du mit {age} zu alt für Lieblingsessen?...")
