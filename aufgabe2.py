d621d6b57a4aa44abd37b283aaf749f553ec387a
benediktbrenk <benedikt.brenk@outlook.de>
2025-01-14 15:09:39 +0100

    Aufgabe 2

---
+++ aufgabe2.py
@@ -0,0 +1,4 @@
+temp_c = input("Bitte geben Sie die Temperatur in Celsius ein: ")
+temp_f = 1.8 * float(temp_c) + 32
+
+print(f"Die Temperatur {temp_c} Grad Celsius entspricht {temp_f} Grad Fahrenheit.")
