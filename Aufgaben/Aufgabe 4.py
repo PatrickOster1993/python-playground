c27ac4ad90895bc6d12344770a8b33a78b2e7c3d
Maze <1932172085@bildung.daa.de>
2025-01-14 14:55:58 +0100

    changed stop parameter 'Aufgabe 4.py'

--- Aufgaben/Aufgabe 4.py
+++ Aufgaben/Aufgabe 4.py
@@ -4,36 +4,31 @@
 
 # > *Hinweis*: Überlegen Sie sich eine sinnvolle Möglichkeit, wie Sie auch Texte über mehrere Zeilen eingeben können!
 
-def replace_and_save():
-    # Eingabe des Textes über mehrere Zeilen
-    print("Geben Sie den Text ein (mit 'stop' beenden):")
-    
-    text = ""
-    
-    while True:
-        # Jede Zeile einlesen
-        line = input()
-        
-        # Beenden der Eingabe, wenn 'stop' eingegeben wird
-        if line.lower() == "stop":
-            break
-        
-        # Hängt die Zeile an den Text an
-        text += line + "\n"
-    
-    # Ersetzen des Punktes durch "SNAKE_WAS_HERE"
-    modified_text = text.replace(".", "SNAKE_WAS_HERE")
-    
-    # Ändern des Pfades: Hier wird die Datei im spezifischen Verzeichnis gespeichert
-    output_path = "c:/Users/IT-User/python-playground/Aufgaben/snake.txt"
+print("Geben Sie den Text ein (mit 'stop' beenden):")
 
-    # Speichern des bearbeiteten Textes in der Datei
-    # Der bearbeitete Text wird in einer Datei namens snake.txt gespeichert
-    # Die Datei wird im Schreibmodus ("w") geöffnet. Falls die Datei bereits existiert, wird sie überschrieben
-    with open(output_path, "w") as file:
-        file.write(modified_text)
-    
-    print("Text wurde erfolgreich bearbeitet und in 'snake.txt' gespeichert.")
+text = ""
 
-# Aufruf der Funktion
-replace_and_save()
+while True:
+    # Jede Zeile einlesen
+    line = input()
+    
+    # Beenden der Eingabe, wenn 'stop' eingegeben wird
+    if line.lower() == "!stop":
+        break
+    
+    # Hängt die Zeile an den Text an
+    text += line + "\n"
+
+# Ersetzen des Punktes durch "SNAKE_WAS_HERE"
+modified_text = text.replace(".", "SNAKE_WAS_HERE")
+
+# Ändern des Pfades: Hier wird die Datei im spezifischen Verzeichnis gespeichert
+output_path = "c:/Users/IT-User/python-playground/Aufgaben/snake.txt"
+
+# Speichern des bearbeiteten Textes in der Datei
+# Der bearbeitete Text wird in einer Datei namens snake.txt gespeichert
+# Die Datei wird im Schreibmodus ("w") geöffnet. Falls die Datei bereits existiert, wird sie überschrieben
+with open(output_path, "w") as file:
+    file.write(modified_text)
+
+print("Text wurde erfolgreich bearbeitet und in 'snake.txt' gespeichert.")
--- Aufgaben/Aufgabe 5.py
+++ Aufgaben/Aufgabe 5.py
@@ -1,5 +1,76 @@
 # ### Aufgabe 5 (freiwillig)
-# Innerhalb der **playground.py** befindet sich eine Codesequenz, unterhalb des Kommentars mit dem Titel "Experten (nicht erforderlich)". Versuchen Sie den Code selbständig nachzuvollziehen und machen Sie sich ggf. Notizen!
+# Innerhalb der **playground.py** befindet sich eine Codesequenz, unterhalb des Kommentars mit dem Titel "Experten (nicht erforderlich)". 
+# Versuchen Sie den Code selbständig nachzuvollziehen und machen Sie sich ggf. Notizen!
 
 # > *Hinweis*: Die kreativsten Lösungen erlangen stets grenzenlose Ehre!
 
+# Listen
+zahlen_liste = [1, 2, 3, 4, 5]
+print("Liste:", zahlen_liste[1])
+
+# # List Comprehension -> verdopellt die Werte in der Liste
+# verdoppelte_werte = [num * 2 for num in zahlen_liste]
+# print("Verdoppelte Werte:", verdoppelte_werte)
+
+
+# # Lambda-Funktion -> quasi dasselbe wie das dadrunter:
+# quadrieren = lambda num: num * num
+# print("Quadrat von 4 (Lambda-Funktion):", quadrieren(4))
+
+# def quadrieren(num):
+#     return num * num
+# print("Quadrat von 4 (allgemeine Funktion):", quadrieren(4))
+
+
+# filter (Liste mit Bedingung filtern)
+gefilterte_zahlen = list(filter(lambda num: num > 3, zahlen_liste))
+print("Gefilterte Zahlen > 3:", gefilterte_zahlen)
+
+# # Eigene Fehler mit raise
+# def check_positive(value):
+#     if value < 0:
+#         raise ValueError(f"Wert muss positiv sein, aber {value} wurde übergeben.")
+#     return value
+
+# try:
+#     check_positive(-1)
+# except ValueError as e:
+#     print("Fehler erkannt:", e)
+
+# # Switch-Case Alternative
+# # Hinweis: Python hat kein echtes switch-case; Dictionary wird verwendet
+# operation = {
+#     "add": lambda a, b: a + b,
+#     "subtract": lambda a, b: a - b,
+#     "multiply": lambda a, b: a * b,
+#     "divide": lambda a, b: a / b if b != 0 else "Division durch Null!",
+# }
+
+# op = "add"
+# result = operation[op](10, 5)
+# print(f"Operation {op}: Ergebnis =", result)
+
+# # Dekoratoren
+# def debug(func):
+#     def wrapper(*args, **kwargs):
+#         print(f"Aufruf von {func.__name__} mit {args}, {kwargs}")
+#         result = func(*args, **kwargs)
+#         print(f"Ergebnis: {result}")
+#         return result
+#     return wrapper
+
+# @debug
+# def multiplizieren(a, b):
+#     return a * b
+
+# multiplizieren(3, 4)
+
+# # Generatoren und yield
+# def zahlen_generator():
+#     for i in range(5):
+#         yield i
+
+# for zahl in zahlen_generator():
+#     print("Generator-Zahl:", zahl)
+
+# # Wichtige und häufig verwendete Libs in Python (folgt)
