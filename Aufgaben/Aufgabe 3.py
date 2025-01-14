5acdd965830c7f4fbb8e591630a1b7a5862bb63a
Maze <1932172085@bildung.daa.de>
2025-01-14 13:47:28 +0100

    created 'Aufgabe 3.py'

---
+++ Aufgaben/Aufgabe 3.py
@@ -0,0 +1,6 @@
+# ### Aufgabe 3
+# Implementieren Sie das klassische **FizzBuzz-Spiel**. Schreiben Sie hierzu ein Programm, das die Zahlen von 1 bis 100 ausgibt, aber für Vielfache von 3 soll es **„Fizz“** ausgeben,
+# für Vielfache von 5 **„Buzz“** und für Vielfache von sowohl 3 als auch 5 **„FizzBuzz“**.
+
+# > **Tipp**: Wie wäre es mit dem Modulo-Operator?
+
