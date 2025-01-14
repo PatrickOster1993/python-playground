b95301440e834a48bdc9cada65eb0408a3705932
Schneider <schneideralex318@gmail.com>
2025-01-14 15:04:23 +0100

    Aufgabe 3 added

--- Aufgaben 14.01.25/Aufgabe3.py
+++ Aufgaben 14.01.25/Aufgabe3.py
@@ -3,3 +3,16 @@
 # das die Zahlen von 1 bis 100 ausgibt, aber für Vielfache von 3 soll es „Fizz“ ausgeben,
 #  für Vielfache von 5 „Buzz“ und für Vielfache von sowohl 3 als auch 5 „FizzBuzz“.
 
+for zahl in range(1, 101):
+
+    if zahl % 3 == 0:
+        print("Fizz")
+
+    if zahl % 5 == 0:
+        print("Buzz")
+
+    if zahl % 3 == 0 and zahl % 5 ==0:
+        print("FizzBuzz")
+
+    else:
+        print(zahl)
---
+++ Aufgaben 14.01.25/Aufgabe4.py
@@ -0,0 +1,5 @@
+# Aufgabe 4
+#Schreiben Sie eine Funktion, die Ihnen einen längeren Text (z. B. die im heutigen Unterricht erlernten Themen)
+# über die Konsole einliest, jeden **"." (Punkt)** durch **"SNAKE_WAS_HERE"** ersetzt und
+# den bearbeiteten Text dann in einer **snake.txt** abspeichert.
+
