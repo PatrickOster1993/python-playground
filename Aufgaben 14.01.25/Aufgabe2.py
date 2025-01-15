7fb2c95a03e74d0cebe36be70dec01a0e405d99c
Schneider <schneideralex318@gmail.com>
2025-01-15 11:26:25 +0100

    Aufg2 optimiert mit Fehlerbehebung und option beenden

--- Aufgaben 14.01.25/Aufgabe2.py
+++ Aufgaben 14.01.25/Aufgabe2.py
@@ -3,29 +3,44 @@
 # Aufgabe 2 Schreiben Sie ein Programm, das eine Temperatur in Celsius in Fahrenheit umrechnet und dann anschließend
 # das Ergebnis in der Konsole ausgibt. Die Umrechnungsformel lautet wie folgt: F = 1.8 * C + 32
 
-def fahrenheit_zu_celsius(fahrenheit):
-    return((fahrenheit-32)/1.8)
+while True:
 
-def celcius_zu_fahrenheit(celsius):
-    return(1.8*celsius+32)
+    try:
 
-print("Celsius - Fahrenheit Rechner:")
-print("1. Fahrenheit zu Celsius")
-print("2. Celsius zu Fahrenheit")
-Auswahl = input("Wie möchten Sie fortfahren? (1. oder 2.)")
+        def fahrenheit_zu_celsius(fahrenheit):
+            return((fahrenheit-32)/1.8)
 
-if Auswahl =="1":
+        def celsius_zu_fahrenheit(celsius):
+            return(1.8*celsius+32)
 
-    fahrenheit = float(input("Temp. in Fahrenheit eingeben."))
-    celsius = fahrenheit_zu_celsius(fahrenheit)
-    print("Die Temp. in Celsius betrifft:", celsius, "Grad.")
+        print("Celsius - Fahrenheit Rechner:")
+        print("1. Fahrenheit zu Celsius")
+        print("2. Celsius zu Fahrenheit")
+        print("3. Prgramm beenden.")
+        Auswahl = input("Wie möchten Sie fortfahren? (1. 2. oder 3.)")
 
-elif Auswahl =="2":
+        if Auswahl =="1":
 
-    celsius = float(input("Temp. in Celcius eingeben."))
-    fahrenheit = celcius_zu_fahrenheit(celsius)
-    print("Die Temp. in Fahrenheit betrifft:", fahrenheit, "Grad.")
+            fahrenheit = float(input("Temp. in Fahrenheit eingeben."))
+            celsius = fahrenheit_zu_celsius(fahrenheit)
+            print("Die Temp. in Celsius betrifft:", celsius, "Grad.")
 
-else:
-    print("Ungültige Eingabe.")
+        elif Auswahl =="2":
+
+            celsius = float(input("Temp. in Celcius eingeben."))
+            fahrenheit = celsius_zu_fahrenheit(celsius)
+            print("Die Temp. in Fahrenheit betrifft:", fahrenheit, "Grad.")
+
+        elif Auswahl =="3":
+            print("Programm beendet.")
+            break
+
+        else:
+            print("Ungültige Eingabe.")
+
+    except ValueError:
+        print("Fehler, gültige Zahl eingeben")
+
+    except Exception as e:
+        print(f"Ein unerwahrteter Fehler ist aufgetreten: {e}")
 
