248579ec70b568cfc0ef227c3232776f0b5b0774
Mattijn <mattijnwitteveen@gmail.com>
2025-01-15 13:34:19 +0100

    Überarbeitung aufgaben

--- selbstlernphase.py
+++ selbstlernphase.py
@@ -1,5 +1,21 @@
-name = input("Gib deinen Namen ein: ")
+""" name = input("Gib deinen Namen ein: ")
 age = input("Gib dein Alter ein: ")
 fav_food = input("Gib dein Lieblingsessen ein: ")
 
 print(f"Hallo {name}, wie gehts? Hast du die letzte Zeit nog {fav_food} gegessen oder bist du mit {age} zu alt für Lieblingsessen?...")
+ """
+
+person_data = {
+    "Name": "",
+    "Age": "",
+    "Fav_food": ""
+}
+
+person_data["Name"] = input("Wie heißt du? ")
+person_data["Age"] = input("Wie alt bist du? ")
+person_data["Fav_food"] = input("Was ist dein Lieblingsessen? ")
+
+print(f"Hallo {person_data['Name']}, wie gehts? Hast du die letzte Zeit nog {person_data['Fav_food']} gegessen oder bist du mit {person_data['Age']} zu alt für Lieblingsessen?...")
+
+
+
--- selbstlernphase2.py
+++ selbstlernphase2.py
@@ -1,4 +1,4 @@
-def temprechnerF_C():
+""" def temprechnerF_C():
     return (degrees -32) / 1.8
 
 def temprechnerC_F():
@@ -21,4 +21,41 @@ elif choice == "2":
     print(f"{input_degrees} Celsius ist {output} Fahrenheit.")
 
 else:
-    print("Bitte 1 oder 2 eingeben.")
+    print("Bitte 1 oder 2 eingeben.") """
+
+temp_rechner = {
+    "1": lambda degrees: (degrees -32) / 1.8,
+    "2": lambda degrees: degrees * 1.8 + 32,
+}
+
+grad_anzeige = {
+    "1": "Celsius",
+    "2": "Fahrenheit",
+}
+
+print("Temperatur Umrechner")
+print("Welche umrechner brauchen Sie?")
+print("1: Fahrenheit nach Normal")
+print("2: Normal nach Fahrenheit")
+choice = input("Ihre Entscheidung lautet: ")
+isFloat = False
+
+while(isFloat == False):
+    try:
+        degrees = float(input("Und wieviel Grad? "))
+        isFloat = True
+    except ValueError:
+        print("Keine Zahl angegeben")
+    result = temp_rechner[choice](degrees)
+
+try:
+    if choice == "1":
+        print(f"{degrees} Grad in {grad_anzeige["2"]} ist {result} in {grad_anzeige['1']}.")
+    elif choice == "2":
+        print(f"{degrees} Grad in {grad_anzeige["1"]} ist {result} in {grad_anzeige['2']}.")
+    else:
+        print("Bitte 1 oder 2 eingeben.")
+except ValueError:
+    print("Bitte ein Zahl eingeben.")
+except Exception:
+    print("Unerwartete Kritische Fehler, Ihre Festplatte wird jetzt gelöscht.")
