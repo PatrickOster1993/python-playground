6ecdd0b910935dd4120204fc55cad020181c97dc
benediktbrenk <benedikt.brenk@outlook.de>
2025-01-15 10:44:13 +0100

    remove text

--- aufgabe4.py
+++ aufgabe4.py
@@ -1,8 +1,3 @@
-# ### Aufgabe 4
-# Schreiben Sie eine Funktion, die Ihnen einen längeren Text (z. B. die im heutigen Unterricht erlernten Themen) über die Konsole einliest, jeden **"." (Punkt)** durch **"SNAKE_WAS_HERE"** ersetzt und den bearbeiteten Text dann in einer **snake.txt** abspeichert.
-
-# > *Hinweis*: Überlegen Sie sich eine sinnvolle Möglichkeit, wie Sie auch Texte über mehrere Zeilen eingeben können!
-
 text = ""
 while True:
     line = input("Gib eine Zeile Text ein (leer lassen zum Beenden): ")
