35ae62a093cd06f59b3617c8feb3ace2c212b763
GentheUgly <roy.faust.b@gmail.com>
2025-01-15 23:52:34 +0100

    NumPy.py + Array + Berechnung und print & numpy import schwierigkeiten auf dem Laptop

---
+++ Aufgaben_2/NumPy.py
@@ -0,0 +1,34 @@
+"""
+### 1. NumPy:
+
+- Erstelle ein NumPy-Array mit den Temperaturen (in °C) der Woche: `[12, 15, 14, 10, 9, 13, 11]`.
+
+- Berechne:
+
+- die durchschnittliche Temperatur,
+
+- die maximale Temperatur,
+
+- die minimale Temperatur.
+"""
+
+#libary import
+import numpy as np
+
+# Temperaturdaten
+temperaturen = np.array([12, 15, 14, 10, 9, 13, 11])
+
+# Berechnungen der Temperatur
+durchschnitt = np.mean(temperaturen)
+max_temp = np.max(temperaturen)
+min_temp = np.min(temperaturen)
+
+# Ausgabe
+print(f"Durchschnittstemperatur: {durchschnitt:.2f}°C")
+print(f"Maximaltemperatur: {max_temp}°C")
+print(f"Minimaltemperatur: {min_temp}°C")
+
+# Output Ergebnisse sollen so aussehn
+# Durchschnittstemperatur: 12.00°C
+# Maximaltemperatur: 15°C
+# Minimaltemperatur: 9°C
