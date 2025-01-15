d3d5988e4981f6f56da4983504f62d8bf22a858d
Schneider <schneideralex318@gmail.com>
2025-01-15 11:13:32 +0100

    Aufg. 4 snake.txt added

--- Aufgaben 14.01.25/Aufgabe4.py
+++ Aufgaben 14.01.25/Aufgabe4.py
@@ -2,11 +2,18 @@
 #Schreiben Sie eine Funktion, die Ihnen einen längeren Text (z. B. die im heutigen Unterricht erlernten Themen)
 # über die Konsole einliest, jeden **"." (Punkt)** durch **"SNAKE_WAS_HERE"** ersetzt und
 # den bearbeiteten Text dann in einer **snake.txt** abspeichert.
+try:
 
-user_input = input("Tragen Sie Ihre Gedanken ein.")
+    user_input = input("Tragen Sie Ihre Gedanken ein.")
 
-neuer_text = user_input.replace(".", "SNAKE_WAS_HERE")
+    neuer_text = user_input.replace(".", "SNAKE_WAS_HERE")
 
-print("Ersetzter Text:", neuer_text)
+    print("Ersetzter Text:", neuer_text)
 
-#muss noch in snake.txt gespeichert werden 
+    with open("snake.txt", "w", encoding="utf-8") as file:
+        file.write(neuer_text)
+
+    print("Ihr Text wurde hoffentlich in 'snake.txt' gespeichert...")
+
+except Exception as e:
+    print(f"Ein fehler ist aufgetreten: {e}")
