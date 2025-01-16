93f5e5fcaae676e5cc2a61a7542affe404d8b376
Blockchainnewbie <haberland-m-d@outlook.com>
2025-01-16 14:24:52 +0100

    Battelfield_warIsOver

--- oop/battlefield.py
+++ oop/battlefield.py
@@ -1,19 +1,108 @@
-from klasse import Krieger
+from klasse import Krieger, Heiler, Magier, Paladin
+import os
+import sys
+import keyboard
+import time
 
-# Beispiel: Erstellen von zwei Kriegern und ein Kampf
-krieger1 = Krieger(name="Herbert", lebenspunkte=100, angriffskraft=20, ruestung=10)
-krieger2 = Krieger(name="Maximilian", lebenspunkte=80, angriffskraft=25, ruestung=15)
+def clear_screen():
+    """Löscht die Konsole"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-# Krieger 1 greift Krieger 2 an
-krieger1.angreifen(gegner=krieger2)
+def wait_for_input():
+    """Wartet auf Leertaste-Eingabe"""
+    print("\nDrücke die LEERTASTE zum Fortfahren...")
+    keyboard.wait('space')  # Wartet bis Leertaste gedrückt und losgelassen wird
+    clear_screen()
+    time.sleep(0.1)  # Kurze Verzögerung um versehentliches Überspringen zu verhindern
 
-# Krieger 2 verteidigt sich
-krieger2.verteidigen()
+# Charaktere erstellen
+heroes = [
+    Krieger(name="Herbert", lebenspunkte=100, angriffskraft=20, ruestung=10),
+    Krieger(name="Maximilian", lebenspunkte=80, angriffskraft=25, ruestung=15),
+    Heiler("Merlin", 100, 20, 10, 30),
+    Magier("Gandalf", 80, 15, 5, 25),
+    Paladin("Arthur", 120, 30, 20, 15, 10)
+]
 
-# Status von Krieger 2 checken
-krieger2.checkStatus()
+# Storyline
+clear_screen()
+print("""
+==========================================
+          Die Schlacht der Helden
+==========================================
 
-# Beispiel für Setter-Methoden
-krieger2.lebenspunkte = 90  # Lebenspunkte ändern
-krieger2.angriffskraft = 30  # Angriffskraft ändern
-krieger2.ruestung = 20      # Rüstung ändern
+In einer fernen Welt bereiten sich die Helden
+auf eine epische Schlacht vor...
+""")
+wait_for_input()
+
+print("""
+Unsere Helden:
+
+1. Herbert - Der tapfere Krieger
+2. Maximilian - Der starke Krieger  
+3. Merlin - Der weise Heiler
+4. Gandalf - Der mächtige Magier
+5. Arthur - Der edle Paladin
+""")
+wait_for_input()
+
+print("""
+Die Schlacht beginnt!
+
+Herbert greift Maximilian an!
+""")
+wait_for_input()
+
+# Kampfsequenz 1
+heroes[0].angreifen(heroes[1])
+wait_for_input()
+
+print("""
+Maximilian verteidigt sich!
+""")
+wait_for_input()
+
+heroes[1].verteidigen()
+wait_for_input()
+
+print("""
+Merlin eilt zur Hilfe und heilt Maximilian!
+""")
+wait_for_input()
+
+heroes[2].heilen(heroes[1])
+wait_for_input()
+
+print("""
+Gandalf betritt das Schlachtfeld und wirft
+einen mächtigen Feuerball!
+""")
+wait_for_input()
+
+heroes[3].feuerball(heroes[0])
+wait_for_input()
+
+print("""
+Arthur reitet in die Schlacht und zeigt seine
+Kraft als Paladin!
+""")
+wait_for_input()
+
+heroes[4].reiten()
+heroes[4].feuerball(heroes[1])
+wait_for_input()
+
+print("""
+==========================================
+          Finaler Status der Helden
+==========================================
+""")
+for hero in heroes:
+    hero.checkStatus()
+    print()
+
+print("""
+Die Schlacht ist vorbei...
+Die Helden kehren erschöpft nach Hause zurück.
+""")
--- oop/klasse.py
+++ oop/klasse.py
@@ -79,3 +79,92 @@ class Krieger:
         print("Angriffskraft: ", self.angriffskraft)
         print("Rüstung: ", self.ruestung)
         print(f"##########################################")
+
+
+class Heiler(Krieger):
+    def __init__(self, name, lebenspunkte, angriffskraft, ruestung, heilkraft):
+        """Initialisiert einen Heiler mit zusätzlicher Heilkraft."""
+        super().__init__(name, lebenspunkte, angriffskraft, ruestung)
+        self.heilkraft = heilkraft
+
+    def heilen(self, ziel):
+        """Heilt einen anderen Krieger oder sich selbst."""
+        if ziel.lebenspunkte <= 0:
+            print(f"{ziel.name} ist bereits besiegt und kann nicht geheilt werden!")
+            return
+
+        heilung = self.heilkraft
+        ziel.lebenspunkte += heilung
+        print(f"{self.name} heilt {ziel.name} um {heilung} Lebenspunkte!")
+        print("#########################################################")
+
+    def checkStatus(self):
+        """Zeigt den Status des Heilers an, inklusive Heilkraft."""
+        super().checkStatus()
+        print("Heilkraft: ", self.heilkraft)
+        print(f"##########################################")
+
+
+class Magier(Krieger):
+    def __init__(self, name, lebenspunkte, angriffskraft, ruestung, magie_kraft):
+        """Initialisiert einen Magier mit zusätzlicher Magiekraft."""
+        super().__init__(name, lebenspunkte, angriffskraft, ruestung)
+        self.magie_kraft = magie_kraft
+
+    def zaubern(self, ziel):
+        """Verwendet Magie, um einem Ziel Lebenspunkte zu geben oder zu nehmen."""
+        if self.magie_kraft > 0:
+            if ziel.lebenspunkte <= 0:
+                print(f"{ziel.name} ist bereits besiegt und kann nicht verzaubert werden!")
+                return
+            
+            effekt = self.magie_kraft
+            ziel.lebenspunkte += effekt
+            print(f"{self.name} verzaubert {ziel.name} und verändert die Lebenspunkte um {effekt}!")
+            print("#########################################################")
+        else:
+            print(f"{self.name} hat keine Magiekraft mehr!")
+            print("#########################################################")
+
+    def verschwinden(self):
+        """Lässt den Magier verschwinden und regeneriert etwas Lebenspunkte."""
+        self.lebenspunkte += 10
+        print(f"{self.name} verschwindet und regeneriert 10 Lebenspunkte!")
+        print("#########################################################")
+
+    def feuerball(self, ziel):
+        """Wirft einen Feuerball auf das Ziel und verursacht Schaden."""
+        schaden = self.magie_kraft * 2 - ziel.ruestung
+        if schaden > 0:
+            ziel.lebenspunkte -= schaden
+            print(f"{self.name} wirft einen Feuerball auf {ziel.name} und fügt {schaden} Schaden zu!")
+            print("#########################################################")
+        else:
+            print(f"{self.name}s Feuerball hat keinen Schaden verursacht!")
+            print("#########################################################")
+
+    def checkStatus(self):
+        """Zeigt den Status des Magiers an, inklusive Magiekraft."""
+        super().checkStatus()
+        print("Magiekraft: ", self.magie_kraft)
+        print(f"##########################################")
+
+
+class Paladin(Magier):
+    def __init__(self, name, lebenspunkte, angriffskraft, ruestung, magie_kraft, reiter_kraft):
+        """Initialisiert einen Paladin mit zusätzlicher Reiterkraft."""
+        super().__init__(name, lebenspunkte, angriffskraft, ruestung, magie_kraft)
+        self.reiter_kraft = reiter_kraft
+
+    def reiten(self):
+        """Der Paladin reitet und erhöht seine Angriffskraft und Rüstung."""
+        self.angriffskraft += self.reiter_kraft
+        self.ruestung += self.reiter_kraft
+        print(f"{self.name} reitet und erhöht Angriffskraft und Rüstung um {self.reiter_kraft}!")
+        print("#########################################################")
+
+    def checkStatus(self):
+        """Zeigt den Status des Paladins an, inklusive Reiterkraft."""
+        super().checkStatus()
+        print("Reiterkraft: ", self.reiter_kraft)
+        print(f"##########################################")
