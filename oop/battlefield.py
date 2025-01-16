44ec5751f1cbefc9df9a1c29b0578ab228ac8dd2
Schneider <schneideralex318@gmail.com>
2025-01-16 15:03:06 +0100

    .

--- oop/battlefield.py
+++ oop/battlefield.py
@@ -1,18 +1,18 @@
 from klasse import Krieger
-
+from klasse import Magier
 # Beispiel: Erstellen von zwei Kriegern und ein Kampf
 krieger1 = Krieger(name="Herbert", lebenspunkte=100, angriffskraft=20, ruestung=10)
-krieger2 = Krieger(name="Maximilian", lebenspunkte=80, angriffskraft=25, ruestung=15)
+magier = Magier(name="Gandalf", lebenspunkte=50, angriffskraft=100, ruestung=1)
 
 # Krieger 1 greift Krieger 2 an
 krieger1.angreifen(gegner=krieger2)
 
 # Krieger 2 verteidigt sich
-krieger2.verteidigen()
+magier.verteidigen()
 
 # Status von Krieger 2 checken
 krieger2.checkStatus()
-
+magier.checkStatus()
 # Beispiel für Setter-Methoden
 krieger2.lebenspunkte = 90  # Lebenspunkte ändern
 krieger2.angriffskraft = 30  # Angriffskraft ändern
