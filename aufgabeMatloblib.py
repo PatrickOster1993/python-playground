39a2f146751a93ca0099ea10223091db8effe319
Mete-Aydin <mete.aydin180395@gmail.com>
2025-01-16 09:44:28 +0100

    Update

--- aufgabeMatloblib.py
+++ aufgabeMatloblib.py
@@ -1,1 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Deaktiviere den interaktiven Modus
+plt.ioff()
+
+
+
+plt.xlabel("Temperaturen der Woche")
+
+names = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+values = [12, 15, 14, 10, 9, 13, 11]
+
+# Erstelle das Diagramm
+plt.plot(names, values)
+
+# Zeige das Diagramm an
+plt.show()
 
