b695a5841ec2f76def9fce549542abceaea7cca2
Schneider <schneideralex318@gmail.com>
2025-01-23 08:33:48 +0100

    test

--- AufgabenSchneider/RollenSchneider/Klasse.py
+++ AufgabenSchneider/RollenSchneider/Klasse.py
@@ -1,7 +1,11 @@
-class Spieler:
-    def __init__ (self):
+class Krieger:
+    def __init__ (self, name, HP, Attack, Gear):
     self.name = name
     self.HP = 0
     self.Attack = 0
     self.Gear = 0
 
+def Status():
+    print(f"{name} has {HP} HP, Gear: {Gear} ")
+
+    
